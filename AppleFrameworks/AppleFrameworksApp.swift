d2bb2a3a51fa69e84736f8ee6b33c900f0c66d37
Dmitrii <dimaeselidze@icloud.com>
2024-05-31 16:53:47 +0300

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Dmitrii Eselidze on 31.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
